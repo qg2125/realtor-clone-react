151d2781471d31a445901ec28c8345a9451ef569
Cara Qing Ge <geqing2012@hotmail.com>
2024-03-17 20:10:23 -0400

    fixed an issue in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,7 @@ function Header() {
   const locations = useLocation();
   const navigate = useNavigate();
   function pathMatchRoute(Route) {
-    if (Route === locations.pathname) {
-      return true;
-    }
+    return Route === locations.pathname;
   }
 
   return (
@@ -22,24 +20,30 @@ function Header() {
         <div>
           <ul className="flex space-x-10">
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${
-                pathMatchRoute("/") && "text-black border-b-red-500"
+              className={`py-3 text-sm  cursor-pointer ${
+                pathMatchRoute("/")
+                  ? "text-black border-b-red-500  border-b-[3px] font-semibold"
+                  : "font-semibold text-gray-400 border-b-[3px] border-b-transparent"
               } `}
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${
-                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              className={`py-3 text-sm  cursor-pointer ${
+                pathMatchRoute("/offers")
+                  ? "text-black border-b-red-500  border-b-[3px] font-semibold"
+                  : "font-semibold text-gray-400 border-b-[3px] border-b-transparent"
               } `}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+              className={`py-3 text-sm  cursor-pointer ${
+                pathMatchRoute("/sign-in")
+                  ? "text-black border-b-red-500  border-b-[3px] font-semibold"
+                  : "font-semibold text-gray-400 border-b-[3px] border-b-transparent"
               } `}
               onClick={() => navigate("/sign-in")}
             >
